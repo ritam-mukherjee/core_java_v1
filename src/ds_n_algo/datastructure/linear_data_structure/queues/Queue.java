791f1f3c88380235479b4234182707e1eaf80322
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2017-12-30 00:35:23 +0530

    Dtata structure : Stack & Queue

---
+++ src/ds_n_algo/datastructure/linear_data_structure/queues/Queue.java
@@ -0,0 +1,90 @@
+package ds_n_algo.datastructure.linear_data_structure.queues;
+
+public class Queue {
+
+    /*The Queue class should have a static node field*/
+    private static class Node{
+        /*The node should have one data field and reference of another node field*/
+        private int data;
+        private Node next;
+            /*It should have one constructor so that at the time of Node creation providing value is mandatory*/
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+   /* All que should having one starting Node called "Head" and one ending node called "Tail"*/
+    private Node head; //value should be remove from head
+    private Node tail; //value should be added at tail
+
+
+    /*Method 1 #  isEmpty() ->If head is null return true else return false*/
+    public boolean isEmpty(){
+        if(head==null)
+            return Boolean.TRUE;
+        else
+            return Boolean.FALSE;
+    }
+
+    /*Method 2# peek() -> If que is not empty return head data*/
+    public Integer peek(){
+        if(isEmpty())
+            return null;
+        else
+            return head.data;
+    }
+
+    /*Method 3 # add(data)    -> this will add element at the end of the Queue*/
+    public void add(int data){
+
+        /*create a node with provided data*/
+        Node node=new Node(data);
+
+        /*if tail not null then tell will point new Node and new Node will consider as "tail"*/
+        if(tail!=null){
+            tail.next=node;
+        }
+        tail=node;
+
+
+        /*If in special scenario where head is null, then new node will also consider as head node
+        * This is the situation when queue have only one element*/
+        if(head==null)
+            head=node;
+
+    }
+
+    /*Method 4 # remove() -> elemenete data from head node*/
+    public int remove(){
+
+        /*remove will happen from head so head element's data should select,
+        * and head elements's reference node will consider as new "head" element*/
+        int data=head.data;
+        head=head.next;
+
+        /*if new head element is null then same node will also consider as tail element*/
+        if(head==null)
+            tail=null;
+
+        return data;
+    }
+
+    public void display(){
+        while (tail!=null){
+            System.out.print("| "+head.data+" |->");
+            remove();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Queue queue=new Queue();
+        queue.add(11);
+        queue.add(22);
+        queue.add(33);
+        queue.add(44);
+        queue.add(55);
+
+        queue.display();
+    }
+}
---
+++ src/ds_n_algo/datastructure/linear_data_structure/stacks/Stack.java
@@ -0,0 +1,72 @@
+package ds_n_algo.datastructure.linear_data_structure.stacks;
+
+public class Stack {
+    /*The Queue class should have a static node field*/
+    private static class Node{
+        /*The node should have one data field and reference of another node field*/
+        private int data;
+        private Node next;
+        /*It should have one constructor so that at the time of Node creation providing value is mandatory*/
+        public Node(int data) {
+            this.data = data;
+        }
+    }
+
+
+    private Node top;
+
+    /*Method 1 #  isEmpty() ->If head is null return true else return false*/
+    public boolean isEmpty(){
+        if(top==null)
+            return Boolean.TRUE;
+        else
+            return Boolean.FALSE;
+    }
+
+    /*Method 2# peek() -> If que is not empty return head data*/
+    public int peek(){
+        return top.data;
+    }
+
+    /*Method 3 # push(data)    -> this will push element at the top of the stacks*/
+    public void push(int data){
+        Node node=new Node(data);
+
+        /*new node is now pointing top element and itself will consider as "top" element*/
+        node.next=top;
+        top=node;
+
+    }
+
+    /*Method 4 # pop() -> element's data from top node */
+    public Integer pop(){
+        /*select top elements data*/
+            int data=top.data;
+        /* the next element is now consider as top element*/
+            top=top.next;
+
+            return  data;
+
+    }
+
+    
+    public void display(){
+        while(top!=null){
+
+            System.out.println("[   "+top.data+"    ]");
+            System.out.println("    ||  ");
+            pop();
+        }
+    }
+
+    public static void main(String[] args) {
+        Stack stack= new Stack();
+        stack.push(11);
+        stack.push(22);
+        stack.push(33);
+        stack.push(44);
+        stack.push(55);
+
+        stack.display();
+    }
+}
