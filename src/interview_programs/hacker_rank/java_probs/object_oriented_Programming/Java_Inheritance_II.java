915e9d4a20cadf7828dd883c554a75fbea8d4cd5
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-12 21:30:10 +0530

    Hackerrank  inhetence 2

---
+++ src/interview_programs/hacker_rank/java_probs/object_oriented_Programming/Java_Inheritance_II.java
@@ -0,0 +1,22 @@
+package interview_programs.hacker_rank.java_probs.object_oriented_Programming;
+
+class Arithmetic{
+    public int add(int a,int b){
+        return a+b;
+    }
+}
+class Adder extends Arithmetic{
+    @Override
+    public int add(int a,int b){
+       return super.add(a,b);
+    }
+}
+public class Java_Inheritance_II {
+    public static void main(String[] args) {
+        Adder a=new Adder();
+
+        System.out.println("My superclass is :"+a.getClass().getSuperclass().getName());
+
+        System.out.println(a.add(10,32)+" "+a.add(10,10)+" "+a.add(10,10)+"\n");
+    }
+}
