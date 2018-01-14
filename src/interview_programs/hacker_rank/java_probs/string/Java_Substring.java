d4fcd5edba24edc9133a7037886e9d229a41dce2
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-14 22:36:32 +0530

    HackerRank: Substring

---
+++ src/interview_programs/hacker_rank/java_probs/string/Java_Substring.java
@@ -0,0 +1,34 @@
+package interview_programs.hacker_rank.java_probs.string;
+
+import java.util.Scanner;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+/**
+ * Source      :    HackerRank
+ * URL          :   https://www.hackerrank.com/challenges/java-substring/problem
+ */
+
+public class Java_Substring {
+    public static void main(String[] args) {
+
+        /*utility functions*/
+        Function<String,Integer> intConverter=Integer::parseInt;
+        Consumer<String> printer=System.out::println;
+
+        Scanner scanner=new Scanner(System.in);
+
+        String word=scanner.next();
+        scanner.nextLine();
+        String[] split_index=scanner.nextLine().split(" ");
+
+
+        BiFunction<String,String[],String> splitter=(input, range) -> {
+            return  input.substring(intConverter.apply(range[0]),intConverter.apply(range[1]));
+        };
+
+        printer.accept(splitter.apply(word,split_index));
+
+    }
+}
