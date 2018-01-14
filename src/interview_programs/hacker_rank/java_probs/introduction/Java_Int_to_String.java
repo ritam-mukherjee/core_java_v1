b9a85dbe48719cffd380c89ed3d170c2868383ab
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-14 21:50:37 +0530

    Hackerrank:: Int -> String

---
+++ src/interview_programs/hacker_rank/java_probs/introduction/Java_Int_to_String.java
@@ -0,0 +1,31 @@
+package interview_programs.hacker_rank.java_probs.introduction;
+
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+
+/**
+ * Link     :   https://www.hackerrank.com/challenges/java-int-to-string/problem
+ * Source   :   HackerRank
+ */
+
+public class Java_Int_to_String {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Integer input = scanner.nextInt();
+
+        Function<Integer, String> intToStringConverter = String::valueOf;
+
+
+        Function<Integer, String> intToStringConverter2 = Object::toString;
+
+        Predicate<Integer> checkRange = integer -> integer >= -100 && integer <= 1000;
+
+        String s = checkRange.test(input) ? intToStringConverter.apply(input) : null;
+
+        System.out.println(s);
+    }
+    }
