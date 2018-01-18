fc8f0f424e688978af7cb546741f37272d996f0b
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-18 16:41:09 +0530

    Hackerrank: Array 1st

---
+++ src/interview_programs/hacker_rank/dta_structure_probs/arrays/Arrays_DS.java
@@ -0,0 +1,35 @@
+package interview_programs.hacker_rank.dta_structure_probs.arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.UnaryOperator;
+
+/**
+ * Source   : HackerRank
+ * Link     : https://www.hackerrank.com/challenges/arrays-ds/problem
+ */
+
+public class Arrays_DS {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        int size= scanner.nextInt();
+        int[] array=new int[size];
+        scanner.nextLine();
+        String input=scanner.nextLine();
+        String[] inputs=input.split(" ");
+
+        Function<String,Integer> change=Integer::parseInt;
+        Consumer<Object> printer=System.out::print;
+
+        for(int i=inputs.length-1;i>=0;i--){
+            array[size-i-1]=change.apply(inputs[i]);
+        }
+        for (int i:array) {
+            printer.accept(i);
+            printer.accept(" ");
+        }
+
+    }
+}
