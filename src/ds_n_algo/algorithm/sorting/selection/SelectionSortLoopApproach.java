d96931449b7f052b17edad625f5f31e61eb5369f
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-18 18:45:49 +0530

    Selection Sort Modification

--- src/ds_n_algo/algorithm/sorting/selection/SelectionSortLoopApproach.java
+++ src/ds_n_algo/algorithm/sorting/selection/SelectionSortLoopApproach.java
@@ -1,7 +1,34 @@
 package ds_n_algo.algorithm.sorting.selection;
 
 /**
- * Created by PouRit on 01-11-2017.
+ * SELECTION SORT
+ ---------------------------
+ Sort the data by finding  or SELECT shortest element and swapping it into  the ARRAY in the first unsorted location.
+
+ [Bubble find largest element and moves into its position, SE do opposite, find smallest and move into it's location]
+
+ Traverse :	Left -> right
+ 1. Identify the sorted element.
+ 2.	Put it into the unsorted  position .
+ 3. Traverse to unsorted position and compare with elements either any item is smaller, if so swap is done.
+
+ Algorithm:
+ 1. One for loop traverse from 0th location to n location pinting i;
+ 2. Another for loop traversing i+1 location to n ocation pointing j,considering 'i'th location's element is shortest.
+ 3. If during traversal find any j location's element smaller than 'i't h location's element do swapping.
+
+ 1. Time complexity:
+ Best Case	:O(n^2)
+ Avg Case	: O(n^2
+ Worst Case O(n^2)
+
+ 2. Space Complexity:	O(n^2) [ Not applicable for large unsorted data set ]
+ < Worked directly with input array, efficient when space crunch is there >.
+
+
+ Note:
+ Hybrid of BUBBLE & INSERTION
+ On performance standard better than "Bubble Sort" but worse than "Insertion Sort".
  */
 public class SelectionSortLoopApproach {
 
@@ -9,7 +36,7 @@ public class SelectionSortLoopApproach {
 
         OutterLoop:/*It maintains regular element (n) traversal of array*/
         for (int i = 0; i < arr.length; i++) {
-            /* 'min_index' is alocation which hold minim element of unsorted arraay
+            /* 'min_index' is a location which hold minimum element of unsorted arraay
             *  It's starts assuming ith loction's element is minimum
             * */
             int min_index=i;
