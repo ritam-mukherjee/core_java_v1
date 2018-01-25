f01bc7d9ad80b6ef55ca68cca4549a4eefd5062d
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-26 00:15:13 +0530

    Hackerrank

--- src/concepts/threads/basics/Thread_Program_1.java
+++ src/concepts/threads/basics/Thread_Program_1.java
@@ -5,32 +5,28 @@ import java.util.function.BiConsumer;
 source  :   udemy
  */
 public class Thread_Program_1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         BiConsumer<String,String> print=(color, name) ->
-                System.out.println(color+" Name: "+name+" is rumming");
+                System.out.println(color+" Name: "+name+" is running");
         Runnable runnable_red=() -> {
             for (int i = 0; i < 5; i++) {
-
                 try {
                     Thread.sleep(500);
                     print.accept(ThreadColor.ANSI_RED,Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
         };
 
         Runnable runnable_green=() -> {
             for (int i = 0; i < 5; i++) {
-
                 try {
                     Thread.sleep(500);
                     print.accept(ThreadColor.ANSI_GREEN,Thread.currentThread().getName());
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
-
             }
         };
 
@@ -38,5 +34,7 @@ public class Thread_Program_1 {
         Thread green_thread=new Thread(runnable_green,"GREEN_THREAD");
         red_thread.start();
         green_thread.start();
+        Thread.sleep(1000);
+        System.out.println(ThreadColor.ANSI_CYAN+"main thread is executing");
     }
 }
