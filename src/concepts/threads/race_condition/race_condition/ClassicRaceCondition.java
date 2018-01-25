eb9a732e67fd9c62422b29bf098d91c59e38b4ed
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-25 22:38:52 +0530

    Race Condition:Pluralsight

---
+++ src/concepts/threads/race_condition/race_condition/ClassicRaceCondition.java
@@ -0,0 +1,47 @@
+package concepts.threads.race_condition.race_condition;
+
+import java.util.function.Supplier;
+
+/**
+ * Classic race COndition Program
+ * Source   : PluralSight Course []
+ */
+
+
+class LongWrapper {
+    private long l;
+    public LongWrapper(long l) {this.l = l;}  // parameterize constructor
+    public long getValue() {return l; }  //getter
+    public void increment_value(){l=l+1; } //incrementor
+}
+public class ClassicRaceCondition {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        Supplier<Long> threadSupplier = () -> {
+            LongWrapper longWrapper = new LongWrapper(0L);
+            Runnable runnable = () -> {
+                for (int i = 0; i < 1_000; i++) {
+                    longWrapper.increment_value();
+                }
+            };
+            /*1000 threads we are going to create with same runnable object and start*/
+            Thread[] threads = new Thread[1_000];
+            for (int i = 0; i < threads.length; i++) {
+                threads[i] = new Thread(runnable);
+                threads[i].start();
+            }
+            /*All threads should execute*/
+            for (int i = 0; i < threads.length; i++) {
+                try {
+                    threads[i].join();
+                } catch (InterruptedException e) { e.printStackTrace(); }
+            }
+            return longWrapper.getValue();
+        };
+        System.out.println(threadSupplier.get());
+        System.out.println(threadSupplier.get());
+        System.out.println(threadSupplier.get());
+        System.out.println(threadSupplier.get());
+    }
+}
