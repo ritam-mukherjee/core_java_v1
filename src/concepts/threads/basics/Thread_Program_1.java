7ec45387a08130ca9cb24c50fb26c93965336250
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-01-26 00:02:15 +0530

    Thread_basic_program

---
+++ src/concepts/threads/basics/Thread_Program_1.java
@@ -0,0 +1,40 @@
+package concepts.threads.basics;
+
+import java.util.function.BiConsumer;
+
+public class Thread_Program_1 {
+    public static void main(String[] args) {
+        BiConsumer<String,String> print=(color, name) ->
+                System.out.println(color+" Name: "+name+" is rumming");
+        Runnable runnable_red=() -> {
+            for (int i = 0; i < 5; i++) {
+
+                try {
+                    Thread.sleep(500);
+                    print.accept(ThreadColor.ANSI_RED,Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        Runnable runnable_green=() -> {
+            for (int i = 0; i < 5; i++) {
+
+                try {
+                    Thread.sleep(500);
+                    print.accept(ThreadColor.ANSI_GREEN,Thread.currentThread().getName());
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        };
+
+        Thread red_thread=new Thread(runnable_red,"RED_THREAD");
+        Thread green_thread=new Thread(runnable_green,"GREEN_THREAD");
+        red_thread.start();
+        green_thread.start();
+    }
+}
