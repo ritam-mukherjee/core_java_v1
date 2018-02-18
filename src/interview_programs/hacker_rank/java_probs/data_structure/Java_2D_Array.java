1d39fa9540b075b28bd2d0179cb3b1074695d506
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-02-18 22:08:47 +0530

    PluralSi

--- src/interview_programs/hacker_rank/java_probs/data_structure/Java_2D_Array.java
+++ src/interview_programs/hacker_rank/java_probs/data_structure/Java_2D_Array.java
@@ -1,5 +1,7 @@
 package interview_programs.hacker_rank.java_probs.data_structure;
 
+
+import java.util.Scanner;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
@@ -9,7 +11,39 @@ import java.util.function.Function;
  */
 public class Java_2D_Array {
     public static void main(String[] args) {
-        Integer[][] arr=new Integer[][]{
+
+        Scanner in = new Scanner(System.in);
+        Integer arr[][] = new Integer[6][6];
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 6; j++) {
+                arr[i][j] = in.nextInt();
+            }
+        }
+        Consumer<Integer> printConsumer = System.out::println;
+        Function<Integer[][], Integer> getBigSum = array -> {
+            int max_sum = Integer.MIN_VALUE; // intialize with 0 create problem if all values are negetive
+            for (int i = 0; i < (array.length - 2); i++) {
+                for (int j = 0; j < (array[i].length - 2); j++) {
+                    /*Hour glass sum calculation*/
+                    int sum = (array[i][j] + array[i][j + 1] + array[i][j + 2])
+                            + array[i + 1][j + 1] +
+                            (array[i + 2][j] + array[i + 2][j + 1] + array[i + 2][j + 2]);
+
+                    if (sum > max_sum)
+                        max_sum = sum;
+                }
+            }
+            return max_sum;
+        };
+
+        printConsumer.accept(getBigSum.apply(arr));
+
+
+    }
+}
+
+
+   /*     Integer[][] arr=new Integer[][]{
                 {1, 1, 1, 0, 0, 0},
                 {0, 1, 0, 0, 0, 0},
                 {1, 1, 1, 0, 0, 0},
@@ -27,28 +61,9 @@ public class Java_2D_Array {
                 {0, 0, 0, -2, 0, 0},
                 {0, 0, -1, -2, -4, 0},
         };
-        Consumer<Integer> printConsumer=System.out::println;
-        Function<Integer[][],Integer> getBigSum=array -> {
-            int max_sum=Integer.MIN_VALUE; // intialize with 0 create problem if all values are negetive
-            for (int i = 0; i < (array.length - 2); i++) {
-                for (int j = 0; j < (array[i].length - 2); j++) {
-                    /*Hour glass sum calculation*/
-                    int sum =   (array[i][j]    + array[i][j + 1] + array[i][j + 2])
-                                                + array[i + 1][j + 1] +
-                                (array[i + 2][j] + array[i + 2][j + 1] + array[i + 2][j + 2]);
 
-                    if (sum>max_sum)
-                        max_sum=sum;
-                }
-            }
-            return max_sum;
-        };
-
-        printConsumer.accept(getBigSum.apply(array2));
-
-        int f=1, i=2;
+        /*  int f=1, i=2;
         while(++i<5)
             f*=i;
-        System.out.println(f);
-    }
-}
+        System.out.println(f);*/
+        */
