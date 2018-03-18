a1fc5032ff493b1e61dbd1bf7cf634bd7251a11e
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-03-18 12:42:33 +0530

    Queue_Modification

---
+++ src/ds_n_algo/datastructure/non_linear_data_structure/binary_search_tree/level_order_traversal/queue_approach/Node.java
@@ -0,0 +1,20 @@
+package ds_n_algo.datastructure.non_linear_data_structure.binary_search_tree.level_order_traversal.queue_approach;
+
+/**
+ Source  :   GeekForGeek
+ Link    :   https://www.geeksforgeeks.org/level-order-tree-traversal/
+ */
+
+class Node {
+    /*each node having a data*/
+    int data;
+
+    /*also two child nodes*/
+    Node left;
+    Node right;
+
+    public Node(int data) {
+        this.data = data;
+    }
+
+}
