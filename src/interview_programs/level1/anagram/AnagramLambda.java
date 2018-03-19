f04d6ffc4fc33fb5ff5aa8756ae558487c286ebd
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-03-19 22:37:07 +0530

    Anagram

--- src/interview_programs/level1/anagram/AnagramLambda.java
+++ src/interview_programs/level1/anagram/AnagramLambda.java
@@ -1,40 +1,60 @@
 package interview_programs.level1.anagram;
 
-import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
+import java.util.function.*;
 
 /**
  * Created by PouRit on 01-11-2017.
  */
 public class AnagramLambda {
+
     public static void main(String[] args) {
-        BiFunction<String,String,Boolean> checkAnagram=(word,anagram)->{
-            boolean status=false;
-           if(word.length()!=anagram.length())
-               status=false;
-            else {
+        Supplier<Integer> alphabets=() -> 26;
+        Supplier<Integer> baseAscii=() -> (int)'a';
+        Function<String,char[]> getCharCounter= s -> {
 
-               for (int i = word.length() % 2; i >= 0; --i) {
-                   if (word.charAt(i) != anagram.charAt(anagram.length() - i - 1)) {
-                      status=false;
-                       break;
-                   }
-                   status=true;
-               }
+            char[] char_counter=new char[alphabets.get()];
 
-           }
-            return status;
+            for (char c:s.toLowerCase().toCharArray()
+                    ) {
+                int offset=(int)c-baseAscii.get();
+                char_counter[offset]++;
+            }
+            return char_counter;
         };
 
-        BiConsumer<String,String> printAnagramStatus=(word1,word2)->{
-            System.out.println("Are the word "+word1+" and "+ word2+" anagram?");
-            System.out.println(checkAnagram.apply(word1,word2));
+
+        BiFunction<char[],char[],Boolean> is_array_equal=(chars1, chars2) -> {
+            boolean isEqual=true;
+            BiPredicate<Character,Character> char_equal=(character1, character2) -> character1==character2;
+
+            char_array_comparison:
+            for (int i = 0; i < chars1.length; i++) {
+
+                if(!char_equal.test(chars1[i],chars2[i])){
+                    isEqual=false;
+                    break char_array_comparison;
+
+                }
+            }
+            return isEqual;
+        };
+        BiFunction<String,String,Boolean> checkAnagram=(s1, s2) -> {
+            boolean isanagram=false;
+
+            if(s1.length()!=s2.length())
+                return isanagram;
+
+            else if(s1.equalsIgnoreCase(s2)) {
+                isanagram = true;
+                return isanagram;
+            }else {
+
+                isanagram= is_array_equal.apply(getCharCounter.apply(s1),getCharCounter.apply(s2))?true:false;
+            }
+            return isanagram;
         };
 
-        printAnagramStatus.accept("ritam","matir");
-        printAnagramStatus.accept("ritam","matira");
 
-        printAnagramStatus.accept("ritam","astha");
-
+        System.out.println(checkAnagram.apply("ritam","matin"));
     }
 }
---
+++ src/interview_programs/level1/anagram/AnagramNormal.java
@@ -0,0 +1,69 @@
+package interview_programs.level1.anagram;
+
+import java.util.Scanner;
+
+
+public class AnagramNormal {
+
+    static final int alphabet_counter=26;
+    static final int baseAscii=(int)'a';
+    static char[] getCharCounter(String s){
+
+        char[] char_counter=new char[alphabet_counter];
+
+        for (char c:s.toLowerCase().toCharArray()
+                ) {
+            int offset=(int)c-baseAscii;
+            char_counter[offset]++;
+        }
+        return char_counter;
+    }
+
+    static Boolean is_array_equal(char[] chars1, char[] chars2) {
+        boolean isEqual=true;
+
+
+        char_array_comparison:
+        for (int i = 0; i < chars1.length; i++) {
+
+            if(!(chars1[i]==chars2[i])){
+                isEqual=false;
+                break char_array_comparison;
+
+            }
+        }
+        return isEqual;
+    }
+
+
+    static boolean isAnagram(String s1, String s2) {
+        // Complete the function
+        boolean isanagram=false;
+
+        if(s1.length()!=s2.length())
+            return isanagram;
+
+        else if(s1.equalsIgnoreCase(s2)) {
+            isanagram = true;
+            return isanagram;
+        }else {
+            char[] chars1=getCharCounter(s1);
+            char[] chars2=getCharCounter(s2);
+            isanagram= is_array_equal(chars1,chars2);
+        }
+        return isanagram;
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        String a = scan.next();
+        String b = scan.next();
+        scan.close();
+        boolean ret = isAnagram(a, b);
+        System.out.println( (ret) ? "Anagrams" : "Not Anagrams" );
+
+    }
+
+}
