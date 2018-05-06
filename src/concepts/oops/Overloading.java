09655cdd3c2a2148491863a8f2efb8ee0ca66e54
ritam_mukherjee <ritam_mukherjee@hotmail.com>
2018-05-06 13:49:41 +0530

    Over Loading

---
+++ src/concepts/oops/Overloading.java
@@ -0,0 +1,18 @@
+package concepts.oops;
+
+/**
+ * ============================================================
+ * INCOMPLETE
+ * =============================================================
+ */
+class Box{
+    int length,bredth;
+    public void getDimension(int length,int bredth){
+        System.out.println(length*bredth);
+    }
+
+}
+public class Overloading {
+
+
+}
